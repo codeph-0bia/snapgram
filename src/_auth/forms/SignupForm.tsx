352d98c4c914f034772cebf8fa763cb20e870beb
codeph-0bia <codephobia642@gmail.com>
2023-11-11 16:15:13 +0530

    add sign-up form ui

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,7 +1,133 @@
-import React from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Link } from 'react-router-dom';
+
+// import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  // FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { useForm } from 'react-hook-form';
+import { Button } from '@/components/ui/button';
+import { SignupValidationSchema } from '@/lib/validation';
+import { z } from 'zod';
+import Loader from '@/components/shared/Loader.tsx';
 
 const SignupForm = () => {
-  return <div>SignupForm</div>;
+  const isLoading = false;
+  const form = useForm<z.infer<typeof SignupValidationSchema>>({
+    resolver: zodResolver(SignupValidationSchema),
+    defaultValues: {
+      username: '',
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof SignupValidationSchema>) {
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <div className="sm:w-420 flex-center flex-col">
+        <img src="/assets/images/logo.svg" alt="" />
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+          Create a new account
+        </h2>
+        <p className="text-light-3 small-medium md:base-regular mt-2">
+          To use snapgram enter your details
+        </p>
+
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-5 w-full mt-4"
+        >
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>UserName</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type="email" className="shad-input" {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" className="shad-input" {...field} />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="shad-button_primary">
+            {isLoading ? (
+              <div className="flex-center gap-2">
+                <Loader />
+              </div>
+            ) : (
+              'Sign Up'
+            )}
+          </Button>
+        </form>
+
+        <p className="text-small-regular text-light-2 text-center mt-2">
+          Already have an account?
+          <Link
+            to="/sign-in"
+            className="text-primary-500 text-small-0semibold ml-1"
+          >
+            {' '}
+            Log In
+          </Link>
+        </p>
+      </div>
+    </Form>
+  );
 };
 
 export default SignupForm;
